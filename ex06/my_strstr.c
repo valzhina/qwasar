974c6557ca2bd41d85cf955d6abfffa872c783df
olya piskun <piskun_o@qwasar.io>
2020-07-23 23:31:06 +0000

    ex06

--- ex06/my_strstr.c
+++ ex06/my_strstr.c
@@ -8,7 +8,7 @@ char* my_strstr(char* param_1, char* param_2) {
     int k;
     char* param_temp;
 
-    if (*param_2 == '\0')
+    if (!*param_2)
         return (param_1);
 
     while (param_1[i] != '\0') {
@@ -28,7 +28,7 @@ char* my_strstr(char* param_1, char* param_2) {
 /*
 int main() {
     char* haystack = strdup("zhymis zhasa'dem");
-    char* needle = "zha";
+    char* needle = "";
 
     printf("from the needle till the end: %s\n", my_strstr(haystack, needle));
     return (0);
