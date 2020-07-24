aca447327682421ec48487a1703a7bf8a276edf4
olya piskun <piskun_o@qwasar.io>
2020-07-24 06:27:18 +0000

    ex08

---
+++ ex08/my_is_sort.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#include <stdbool.h>
+#ifndef STRUCT_INTEGER_ARRAY
+#define STRUCT_INTEGER_ARRAY
+typedef struct s_integer_array
+{
+    int size;
+    int* array;
+} integer_array;
+#endif
+
+bool my_is_sort(integer_array* param_1)
+{
+    int i = 1;
+
+    if (param_1->size < 2)
+        return (true);
+    if (param_1->array[i] >= param_1->array[i - 1]) {
+        while (i < param_1->size && param_1->array[i] >= param_1->array[i - 1])
+            i++;
+        if (i == param_1->size)
+            return (true);
+    }
+    i = 1;
+    if (param_1->array[i] <= param_1->array[i - 1]) {
+        while (i < param_1->size && param_1->array[i] <= param_1->array[i - 1])
+            i++;
+        if (i == param_1->size)
+            return (true);
+    }
+    return (false);
+}
+/*
+int main() {
+
+int size = 4;
+int array[] = {6, 6, 6, 6};
+struct s_integer_array ldata = {size, array};
+
+printf("%d\n", my_is_sort(&ldata));
+
+
+return 0;
+}*/
